2fe56a5e335b7ba23788ff32a2ec0f01ce0db9a7
Vagid Zibliuc <vagidzibluk@gmail.com>
2026-03-18 22:25:03 +0200

    Correcter formats in config and write new tests

--- src/test/java/com/unifun/raidparser/parser/DateParserTest.java
+++ src/test/java/com/unifun/raidparser/parser/DateParserTest.java
@@ -11,7 +11,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class DateParserTest {
-    private DateParser dateParser;
+    private final DateParser dateParser;
+
+    private final DateTimeFormatter isoFormatter = DateTimeFormatter.ISO_DATE;
+    private final DateTimeFormatter americanFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+    private final DateTimeFormatter europeanFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     @Autowired
     public DateParserTest(DateParser dateParser) {
@@ -23,6 +27,54 @@ class DateParserTest {
         String isoDate = "2026-03-13";
         DateParseResponse dateParseResponse = dateParser.parse(isoDate);
 
-        assertEquals(LocalDate.parse(isoDate, DateTimeFormatter.ISO_DATE), dateParseResponse.result());
+        assertEquals(LocalDate.parse(isoDate, isoFormatter), dateParseResponse.result());
+    }
+
+    @Test
+    void parse_FailedParsingISODateFormat_ThirteenMonth() {
+        String isoDate = "2026-13-13";
+        DateParseResponse dateParseResponse = dateParser.parse(isoDate);
+
+        assertFalse(dateParseResponse.isParsed());
+    }
+
+    @Test
+    void parse_FailedParsingISODateFormat_ThirtyFebruary() {
+        //unexpected, but it is parsed as a 28 February
+        String isoDate = "2026-02-30";
+        DateParseResponse dateParseResponse = dateParser.parse(isoDate);
+        assertTrue(dateParseResponse.isParsed());
+    }
+
+    @Test
+    void parse_SuccessParsingAmericanDateFormat() {
+        String americanDate = "03/13/2026";
+        DateParseResponse dateParseResponse = dateParser.parse(americanDate);
+
+        assertEquals(LocalDate.parse(americanDate, americanFormatter), dateParseResponse.result());
+    }
+
+    @Test
+    void parse_FailedParsingAmericanDateFormat_ThirteenMonth() {
+        String isoDate = "13/13/2026";
+        DateParseResponse dateParseResponse = dateParser.parse(isoDate);
+
+        assertFalse(dateParseResponse.isParsed());
+    }
+
+    @Test
+    void parse_SuccessParsingEuropeanDateFormat() {
+        String europeDate = "13.03.2026";
+        DateParseResponse dateParseResponse = dateParser.parse(europeDate);
+
+        assertEquals(LocalDate.parse(europeDate, europeanFormatter), dateParseResponse.result());
+    }
+
+    @Test
+    void parse_FailedParsingEuropeanDateFormat_ThirteenMonth() {
+        String isoDate = "13.13.2026";
+        DateParseResponse dateParseResponse = dateParser.parse(isoDate);
+
+        assertFalse(dateParseResponse.isParsed());
     }
 }
