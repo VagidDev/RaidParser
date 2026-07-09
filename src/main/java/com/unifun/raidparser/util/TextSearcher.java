03d7a10471a9f16c8dcf286cf0001391f661376c
Vagid Zibliuc <vagidzibluk@gmail.com>
2026-07-09 12:39:19 +0300

    Add text searcher and test for it

---
+++ src/main/java/com/unifun/raidparser/util/TextSearcher.java
@@ -0,0 +1,35 @@
+package com.unifun.raidparser.util;
+
+public class TextSearcher {
+    public static boolean containsAll(String text, String... words) {
+        if (text == null || words == null || words.length == 0) {
+            return false;
+        }
+
+        for (String word : words) {
+            String regex = "(?i).*\\Q" + word + "\\E.*";
+            if (!text.matches(regex)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean containsAny(String text, String... words) {
+        if (text == null || words == null || words.length == 0) {
+            return false;
+        }
+
+        StringBuilder sb = new StringBuilder("(?i).*(");
+        for (int i = 0; i < words.length; i++) {
+            sb.append("\\Q").append(words[i]).append("\\E");
+            if (i < words.length - 1) {
+                sb.append("|");
+            }
+        }
+        sb.append(").*");
+
+        return text.matches(sb.toString());
+    }
+
+}
