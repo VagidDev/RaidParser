8c61da6d8e4d7282102d649d8bc5fc9597ae6bdb
Vagid Zibliuc <vagidzibluk@gmail.com>
2026-08-19 17:59:15 +0300

    Remove unused code in ServerHealthCheckerService and modified tests for it

--- src/main/java/com/unifun/raidparser/service/ServerHealthCheckService.java
+++ src/main/java/com/unifun/raidparser/service/ServerHealthCheckService.java
@@ -7,17 +7,13 @@ import com.unifun.raidparser.dto.ServerData;
 import com.unifun.raidparser.dto.ServerTask;
 import com.unifun.raidparser.handlers.ServersToCheckConfigFileDataHandler;
 import com.unifun.raidparser.mapper.ServerTaskMapper;
-import com.unifun.raidparser.util.RemoteCommandExecutor;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
-import java.util.AbstractMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -25,23 +21,9 @@ public class ServerHealthCheckService {
     private static final Logger LOGGER = LogManager.getLogger(ServerHealthCheckService.class);
 
     private final ServersToCheckConfigFileDataHandler serversToCheckConfigFileDataHandler;
-    private final RemoteCommandExecutor remoteCommandExecutor;
     private final HostExecutorService hostExecutorService;
-    private final ServersToCheckConfig serversToCheckConfig;
     private final HostOverviewService hostOverviewService;
     private final ServerTaskMapper serverTaskMapper;
-
-    public List<ServerData> checkServers() {
-        List<HostCommand> hostCommands = serversToCheckConfigFileDataHandler.getHostCommands();
-        if (hostCommands == null) {
-            LOGGER.warn("No tasks to check!");
-            return List.of();
-        }
-        Map<HostCommand, HostInformation> hostsToCheck = getHostsToCheck(hostCommands);
-        return hostsToCheck.entrySet().stream().
-                map(entry -> checkServer(entry.getKey(), entry.getValue()))
-                .toList();
-    }
 
     public List<ServerData> checkServersParallel() {
         List<HostCommand> hostCommands = serversToCheckConfigFileDataHandler.getHostCommands();
@@ -57,47 +39,5 @@ public class ServerHealthCheckService {
         LOGGER.info("Got {} servers data after executing", serverData.size());
         return serverData;
     }
-
-    public Map<HostCommand, HostInformation> getHostsToCheck(List<HostCommand> hostCommands) {
-        return hostCommands.stream()
-                .map(serverTask -> new AbstractMap.SimpleEntry<>(
-                        serverTask,
-                        hostOverviewService.getPhysicalServerWithCorrectPortByName(serverTask.getHost())
-                ))
-                .filter(entry -> {
-                    if (entry.getValue() == null) {
-                        LOGGER.warn("Cannot check the server due to null reference received! Server task -> {}, Host information -> {}", entry.getKey(), entry.getValue());
-                        return false;
-                    }
-                    return true;
-                })
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        Map.Entry::getValue
-                ));
-    }
-
-    private ServerData checkServer(HostCommand hostCommand, HostInformation hostInformation){
-        String commandOutput = "";
-        if (hostInformation.getConnectionType().equalsIgnoreCase("proxy"))
-            commandOutput = remoteCommandExecutor.execute(
-                    serversToCheckConfig.getProxyServerIp(),
-                    hostInformation.getPort(),
-                    hostCommand.getCommand()
-            );
-        else
-            commandOutput = remoteCommandExecutor.execute(
-                    hostInformation.getIp(),
-                    22,
-                    hostCommand.getCommand()
-            );
-        LOGGER.debug("Output of the command {} is: {}", hostCommand.getCommand(), commandOutput);
-        return new ServerData(
-                hostCommand.getCommand(),
-                Map.of(hostCommand.getType(), commandOutput)
-        );
-    }
-
-
 
 }
